a8e99ecbfcd9e2572a09bdfdcc1acb97560d8fd5
Vinchands <kevinchandra031@gmail.com>
2024-09-28 12:14:22 +0700

    Adding my own username so that everyone know that this app is mine. Hehe.

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -5,6 +5,9 @@ export default function Title() {
         <div className='text-center'>
             <h1 className="title">Lemme Guess</h1>
             <p className="subtitle">I can read your mind...</p>
+            <span>
+                By <a href='https://github.com/Vinchands'>Vinchands</a> on <a href='https://github.com'>GitHub</a>
+            </span>
         </div>
     )
 }
