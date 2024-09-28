99bed6076a1e5db11e93c98e2ddaca0b8a7ab64a
Vinchands <kevinchandra031@gmail.com>
2024-09-28 10:29:47 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  }
+    base: '/react-lemme-guess/',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    }
 })
